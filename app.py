403c0c60235b84f3e60c4d9512772912db7269da
jaiobrien <jai.obrien@tamu.edu>
2023-02-13 23:27:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from pip._vendor import requests
 from urllib.request import urlretrieve
 import os
 import re
+import collections
 
 # variable initialization
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
@@ -57,30 +58,101 @@ with open(log_file, "r") as file:
 
 
   
+#1. & 2. Daily, Weekly, Monthly
+#(Done by Wesley) The log file began on October 24th 1994 and ended on October 11th 1994 which is 365 days minus 13 days, or 352 days. 
+#All of the following are averages
+dayCalc = request_total / (365-13)
+dayCalc = int(dayCalc)
+#352 days translates to roughly 50 weeks
+weekCalc = request_total / (52 - 2)
+weekCalc = int(weekCalc)
+#50 weeks is rounded up to a full 12 months
+monthCalc = request_total / (12)
+monthCalc = int(monthCalc)
+
+
 
 # TODO: Output for marketing
 print("\nLog Data from AWS")
 print("\nTotal requests from last six months:", last_six_month_request_counter)
 print("\nTotal requests made:", request_total)
-print("\nTotal requests made each day:")
-print("\nTotal requests made each week:")
-print("\nTotal requests made each month:")
+print("\nAverage requests made each day:", dayCalc)
+print("\nAverage requests made each week:", weekCalc)
+print("\nAverage requests made each month:", monthCalc)
 print("\nTotal number of requests made in the last 6 months (Req. Total and Log Line)", request_total - end_date_line)
 
 #TEST
 # month = "Oct"
-# query = ".*\[[0-9]+/(" + month + ")/[0-9]{4}:.* \-[0-9]{4}\] \".*\" .*"  
-   
+# query = ".*\[[0-9]+/(" + month + ")/[0-9]{4}:.* \-[0-9]{4}\] \".*\" .*" 
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"] 
+
+
+
 
 # 5. What was the most-requested file?
 # Responsible: [Brandon]
 
-#.*\[[0-9]+/[a-zA-Z]+/[0-9]{4}:.* \-[0-9]{4}\] (\".*\") .*
+
+def getCountOfFiles(log_file):
+    file_count = {}
+    with open(log_file, 'r') as file:
+        Content = file.read()
+        lines = Content.split("\n")
+
+    for line in lines:
+        data = re.split(r'.*? - .* \[.*?\] \".*? (.*?)\"? .+? .+ .+', line)
+        
+        if len(data) < 3:
+            # if it did not parse correctly, it will re-parse
+            if len(line) > 40:
+                if data[0].count("\"") == 2:
+                    data = re.split(r'.*\[[0-9]+/[a-zA-Z]+/[0-9]{4}:.* \-[0-9]{4}\] \"(?:GET )(.*).+\" .*', line)
+                
+                data = re.split(r'.*\[[0-9]+/[a-zA-Z]+/[0-9]{4}:.* \-[0-9]{4}\] \"(?:GET )(.*).+\ .*', line)
+            else:
+                # short ones like local   index.html
+                data = re.split(r'.*? .+ .*?(.+)', line)
+        
+        if len(data) == 3:
+            # if the data reparsed correctly
+            filename = data[1]
+            if filename in file_count:
+                file_count[filename] += 1
+            else:
+                file_count[filename] = 1
+        
+    return file_count
+    
+# prints dictionary
+# print(getCountOfFiles(log_file))
+file_count = getCountOfFiles(log_file)
+print("Most Requested File Name", max(file_count, key=file_count.get))
+print("Least Requested File Name", min(file_count, key=file_count.get))
+
+
+
+# things = {}
+# for line in open(log_file):
+#    pieces = re.split('.*\[[0-9]+/[a-zA-Z]+/[0-9]{4}:.* \-[0-9]{4}\] (\".*\") .*',line)
+#    filename = pieces(4)
+# if filename in things:
+#     things[filename] += 1
+# else:
+#     things[filename] = 1
+
+# print(filename, 'was the most requested file.'+len(things))
+
 
 # 6. What was the least-requested file? 
 # Responsible: [Brad]
 
+
+
+
 # 7. logs broken into separate files by month
+
+
 # Responsible: Jai and Paula
 Log_pattern = r'(.*?) - (.*) \[(.*?)\] \"(.*?) (.*?)\"? (.+?) (.+) (.+)'   
 with open(log_file, 'r') as file:
@@ -92,8 +164,8 @@ for line in lines:
     if not matches:
         continue
 
-    matches.group(0) 
-    matches.group(3) 
+    
+     
     time = matches.group(3)
     month = time[3:6]
 
@@ -123,7 +195,16 @@ for line in lines:
         dec_file.write(line+'\n')
     else:
         continue
+    
+# Per Month Basis
 
+print("\nTotal requests made each month:")
+for month in months:
+    filename = month + ".txt"
+    with open(filename, "r") as file:
+        print(month, ": ", len(file.readlines()))
+
+#3. & 4. Percentage of requests
 # TODO: Percentage of requests that had an error code or where redirected elsewhere
 error3count = 0
 error4count = 0
@@ -143,3 +224,4 @@ percent3 = ("{:.2f}%".format(error3count/request_total * 100))
 percent4 = ("{:.2f}%".format(error4count/request_total * 100))
 print(percent4, "of requests were not successful")
 print(percent3, "of requests were redirected elsewhere")
+
